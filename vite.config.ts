46dffd84e905a02a0d8e0c554ff578177d242042
ryohei-ebisawa <xebisawa.ryohei@contract.isid.co.jp>
2024-09-19 10:27:44 +0900

    Initialize project

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: {
+        main: './lib/index.ts',
+      },
+      formats: ['es', 'cjs'],
+      name: 'mdoc-cbor-ts',
+      fileName: (format) => {
+        return `${format}/index.js`;
+      },
+    },
+  },
+  test: {
+    globals: true,
+    include: ['lib/**/*.test.ts', 'lib/**/*.spec.ts'],
+    coverage: {
+      provider: 'v8',
+      include: ['lib/**/*.test.ts', 'lib/**/*.spec.ts'],
+      exclude: ['lib/index.ts'],
+    },
+  },
+});
